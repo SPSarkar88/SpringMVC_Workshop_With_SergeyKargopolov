bce6a3f6a10b2aaa40cb75462de1c743f43a5481
Matteo Di Gregorio <matteodgrgr@gmail.com>
2021-04-15 19:21:43 +0100

    add war deployment and external tomcat config

--- src/main/java/com/matteo/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/matteo/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,14 +2,23 @@ package com.matteo.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class RestfulWebServicesApplication {
+/* 'extends' necessary for WAR deployment */
+public class RestfulWebServicesApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(RestfulWebServicesApplication.class, args);
+  }
+
+  /* Necessary for WAR deployment */
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(RestfulWebServicesApplication.class);
   }
 
   @Bean
